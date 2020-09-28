b9ca779df840872231fb5f988fde575fbc654484
Saul Costa <saulcosta18@gmail.com>
2020-09-28 16:45:45 -0700

    Update commands.rb

--- lib/pry-nav/commands.rb
+++ lib/pry-nav/commands.rb
@@ -19,9 +19,9 @@ module PryNav
 
     helpers do
       def breakout_navigation(action, times)
-        _pry_.binding_stack.clear     # Clear the binding stack.
-        throw :breakout_nav, {        # Break out of the REPL loop and
-          :action => action,          #   signal the tracer.
+        pry_instance.binding_stack.clear  # Clear the binding stack.
+        throw :breakout_nav, {            # Break out of the REPL loop and
+          :action => action,              #   signal the tracer.
           :times =>  times
         }
       end
